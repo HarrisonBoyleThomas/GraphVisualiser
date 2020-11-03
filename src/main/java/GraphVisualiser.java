b7dd4a5e6e0c147bb1b2e7afa87980add7e7cedd
Harrison Boyle-Thomas <k1889066@kcl.ac.uk>
2020-11-03 11:41:14 +0000

    Update build.gradle to include javafx. Add main application and main window classes

---
+++ src/main/java/GraphVisualiser.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.scene.*;
+import javafx.stage.Stage;
+
+import menu.*;
+
+public class GraphVisualiser extends Application{
+	public static void main(String[] args){
+		launch(args);
+	}
+	
+	public void start(Stage primaryStage) throws Exception{
+		primaryStage.setTitle("Graph visualiser");
+		
+		primaryStage.setScene(new Scene(makeMainWindow(), 1000, 700));
+		primaryStage.show();
+	}
+	
+	private MainWindow makeMainWindow(){
+		return new MainWindow();
+	}
+	
+}
---
+++ src/main/java/menu/MainWindow.java
@@ -0,0 +1,12 @@
+package menu;
+
+import javafx.scene.layout.BorderPane;
+
+
+public class MainWindow extends BorderPane{
+	
+	public MainWindow(){
+		
+	}
+	
+}
